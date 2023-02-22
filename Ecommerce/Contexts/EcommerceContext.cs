b5fd433a921e3e64b617cfdb5cb1b708a890cc69
fifete <dfuentes@MG-PC019AVERGAR>
2023-02-22 11:29:38 -0500

    Add project files.

---
+++ Ecommerce/Contexts/EcommerceContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Contexts
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext(DbContextOptions<EcommerceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
---
+++ Ecommerce/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Category { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? Image { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Total { get; set; }
+    }
+}
